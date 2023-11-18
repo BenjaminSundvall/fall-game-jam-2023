5f7b6029fc4a0e1914d44a97d64bdddf05d37a60
nobodynr15 <erikseglare@gmail.com>
2023-11-18 16:11:46 +0100

    Added keyframes to baguette

--- godot/Weapon/Sword.gd
+++ godot/Weapon/Sword.gd
@@ -2,12 +2,13 @@ extends Weapon
 
 var dangerZone = []
 var enemy
+var animationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Area2D").connect("body_entered", _on_body_entered)
 	get_node("Area2D").connect("body_exited", _on_body_exited)
+	animationPlayer = get_node("AnimationPlayer")
 	enemy = _get_enemy()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,13 +16,14 @@ func _process(delta):
 	pass
 	
 func attack(direction):
+	animationPlayer.play()
 	for i in range(dangerZone.size()):
 		dangerZone[i].take_damage(40)
 	
 
 func _on_body_entered(body):
-		if(body.is_in_group(enemy)):
-			dangerZone.append(body)
+	if(body.is_in_group(enemy)):
+		dangerZone.append(body)
 	
 
 func _on_body_exited(body):
