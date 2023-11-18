c578f2121b2f3d61fe694a813f1efe90da0a78b4
Zackawa <zackarias.w@gmail.com>
2023-11-18 10:52:16 +0100

    Update enemy.gd

--- godot/enemy.gd
+++ godot/enemy.gd
@@ -7,32 +7,32 @@ var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_node("Player")
+	player = $"../Player" # TODO: Make not direct
+	print_debug(player)
 	attack = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (attack):
-		var player_coord = player.get_position()
-		var self_coord = get_parent().get_position()
-		var direction = (player_coord-self_coord).normalized()
-		velocity = direction * speed
-		move_and_slide()
-	pass
+	if (attack):	
+		_walking(player.position)
+		
+func _walking(to_where):
+	var direction = (to_where-position).normalized()
+	velocity = direction * speed
+	move_and_slide()
 
 func _die():
 	#Potential animations etc for dying
 	self.queue_free()
-	pass
 	
+# Public beahviour call
 func start_attacking():
+	print_debug("True")
 	attack = true
-	pass
 	
 func take_damage(damage):
 	health += damage
 	if health <= 0:
 		self._die()
-	pass
