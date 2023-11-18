024c401efe43ce7eddc6f5cad7446082ba8f1e98
nobodynr15 <erikseglare@gmail.com>
2023-11-19 00:01:42 +0100

    working on boomerang

--- godot/Weapon/CroissantBoomerang.gd
+++ godot/Weapon/CroissantBoomerang.gd
@@ -2,18 +2,42 @@ extends Weapon
 
 
 var enemy
+var holding = true
+var collisionShape
+var min_lifetime = 0.2
+var lifetime = 0
+var currently_on_parent = true
+var speed = 1
+var current_dir
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Area2D").connect("body_entered", _on_body_entered)
 	get_node("Area2D").connect("body_exited", _on_body_exited)
-	enemy = _get_enemy()
+	collisionShape = get_node("Area2D/CollisionShape2D")
+	collisionShape.disabled = true
+	bullet_damage = 50
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	lifetime += delta
+	if(lifetime >= min_lifetime and currently_on_parent and not holding):
+		print("return to papa")
+		holding = true
+		pass
 	pass
-	
+
 func attack(direction):
+	if(holding):
+		holding = false
+		var enemy = _get_enemy()
+		var bullet = Bullet.instantiate()
+		get_node("/root").add_child(bullet)
+		bullet.enemy = enemy
+		bullet.global_position = global_position
+		bullet.direction = direction.normalized()
+		bullet.damage = bullet_damage
+
 	pass
 
 	
@@ -21,12 +45,16 @@ func attack(direction):
 func _on_body_entered(body):
 	if(body.is_in_group(enemy)):
 		body.take_damage(50)
+	if(body == get_parent()):
+		currently_on_parent = true
 
 	
 
 func _on_body_exited(body):
 		if(body.is_in_group(enemy)):
 			pass
+		if(body == get_parent()):
+			currently_on_parent = false
 
 	
 	
