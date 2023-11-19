6f5e9aadc0192e10887d05dc1522dbdec42c9320
nobodynr15 <erikseglare@gmail.com>
2023-11-19 13:39:17 +0100

    boomerang almost working

--- godot/Bullet/BoomerangBullet.gd
+++ godot/Bullet/BoomerangBullet.gd
@@ -1,16 +1,21 @@
 extends Bullet
 
 var player_who_shot
+var on_player
+var min_lifetime
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
 	lifetime = 0
-	speed = 10
+	speed = 5
 
 
 func _process(delta):
 	_movement(direction)
 	lifetime += delta
+	if(lifetime >= min_lifetime and on_player):
+		queue_free()
 
 func _movement(dir):
 	position += dir * speed
@@ -20,3 +25,9 @@ func _movement(dir):
 func _on_body_entered(body):
 	if(body.is_in_group(enemy)):
 		body.take_damage(damage)
+	if(body == player_who_shot):
+		on_player = true
+		
+func _on_body_exited(body):
+	if(body == player_who_shot):
+		on_player = false
--- godot/Weapon/CroissantBoomerang.gd
+++ godot/Weapon/CroissantBoomerang.gd
@@ -6,6 +6,7 @@ var holding = true
 var min_lifetime = 0.2
 var current_dir
 var parent
+var boolet
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bullet_damage = 50
@@ -15,6 +16,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rotation = weapon_vector.angle()
+	if(boolet == null):
+		holding = true
 	pass
 
 func attack(direction):
@@ -23,10 +26,13 @@ func attack(direction):
 		var enemy = _get_enemy()
 		var bullet = Bullet.instantiate()
 		get_node("/root").add_child(bullet)
+		boolet = bullet
 		bullet.enemy = enemy
 		bullet.global_position = global_position
 		bullet.direction = direction.normalized()
 		bullet.damage = bullet_damage
+		bullet.player_who_shot = parent
+		bullet.min_lifetime = min_lifetime
 
 	
 	
